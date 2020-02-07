95c6a40305f55360dadfb2d377809babded325b0
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-08 01:24:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,13 @@
    $browser = $data->getElementsByTagName("browser")->item(0)->nodeValue;
    $creator= $data->getElementsByTagName("creator")->item(0)->nodeValue;
    $publish = $data->getElementsByTagName("publish")->item(0)->nodeValue;
+   $language = $data->getElementsByTagName("language")->item(0)->nodeValue;
   
    echo "<li><b>Browser Name:</b> $browser
             <ul>
                 <li> <b>The Creator:</b> $creator</li>
                 <li> <b>Published:</b> $publish</li>
+                <li> <b>Programming Language:</b> $language</li>
             </ul>
         </li>";
  }
