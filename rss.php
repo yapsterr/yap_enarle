e458e2c5895854715e4734c042f91f7c75570a54
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-08 00:32:25 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -4,19 +4,18 @@ $rss= '<?xml version="1.0" encoding="UTF-8"?>';
 $rss .= '<rss version="2.0">';
 $rss .= '<channel>';
 
-$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820860") or die (mysqli_error($connect));
-$sql = "SELECT * FROM tblballpen";
+$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820335") or die (mysqli_error($connect));
+$sql = "SELECT * FROM tblanime";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
 
 while($record= mysqli_fetch_assoc($query)) {
     extract($record);
     
-    $rss .= '<ballpen>';
-    $rss .= '<name>' . $ballpen_name. '</name>';
-    $rss .= '<price>' . $price . '</price>';
-    $rss .= '<color>' . $color . '</color>';
-    $rss .= '<supplier>' . $supplier . '</supplier>';
+    $rss .= '<anime>';
+    $rss .= '<anime_title>' . $anime_title. '</anime_title>';
+    $rss .= '<anime_author>' . $anime_author . '</anime_author>';
+    $rss .= '<anime_description>' . $anime_description . '</anime_description>';
     $rss .= '</ballpen>';
 }
 $rss .= '</channel>';
