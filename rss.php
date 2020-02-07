e6f4e1ce21a2b11ac7ae0932683410dd44ed4b43
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-08 01:02:32 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -5,18 +5,18 @@ $rss .= '<rss version="2.0">';
 $rss .= '<channel>';
 
 $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820335") or die (mysqli_error($connect));
-$sql = "SELECT * FROM tblanime";
+$sql = "SELECT * FROM tblbrowser";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
 
 while($record= mysqli_fetch_assoc($query)) {
     extract($record);
     
-    $rss .= '<anime>';
-    $rss .= '<title>' . $title. '</title>';
-    $rss .= '<author>' . $author . '</author>';
-    $rss .= '<description>' . $description . '</description>';
-    $rss .= '</anime>';
+    $rss .= '<browser>';
+    $rss .= '<name>' . $name. '</name>';
+    $rss .= '<creator>' . $creator . '</creator>';
+    $rss .= '<publish>' . $publish . '</publish>';
+    $rss .= '</browser>';
 }
 $rss .= '</channel>';
 $rss .= '</rss>';
