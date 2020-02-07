dfc2bda7211dc3b8264a008c99ea2647b628992c
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-07 18:26:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
         <?php
               foreach($content as $data)
         {          
-           $Title = $data->getElementsByTagName("anime_title")->item(0)->nodeValue;
-           $Author = $data->getElementsByTagName("anime_author")->item(0)->nodeValue;
-           $Description = $data->getElementsByTagName("anime_description")->item(0)->nodeValue;
-           echo "<ul><h2>$Title</h2>
+           $animeT = $data->getElementsByTagName("anime_title")->item(0)->nodeValue;
+           $animeA = $data->getElementsByTagName("anime_author")->item(0)->nodeValue;
+           $animeD = $data->getElementsByTagName("anime_description")->item(0)->nodeValue;
+           echo "<ul><h2>$animeT</h2>
                     <ul>
-                        <li>Author: $Author </li>
-                        <li>Description: $Description</li>
+                        <li>Author: $animeA </li>
+                        <li>Description: $animeD</li>
                         
                     </ul>
                 </ul>";
