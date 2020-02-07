dc05532208aab13f8c14bf9370a0f4a2f8645bb0
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-07 18:34:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
-       $domOBJ = new DOMDocument();
-       $domOBJ->load("https://xmlyap.herokuapp.com/rss.php");//XML page URL
-
-       $content = $domOBJ->getElementsByTagName("Anime");
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xmlyap.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("anime");
+ 
+ ?>
+<h2> Anime Informations </h2>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("anime_title")->item(0)->nodeValue;
+   $author = $data->getElementsByTagName("anime_author")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("anime_description")->item(0)->nodeValue;
+   
+   echo "<li><b>Title:</b> $title
+            <ul>
+                <li> <b>Author:</b> $author</li>
+                <li> <b>Description:</b> $description</li>
+            </ul>
+        </li>";
+ }
 ?>
-       
-       <h1>Anime</h1>
-           
-       
-        <?php
-              foreach($content as $data)
-        {          
-           $animeT = $data->getElementsByTagName("anime_title")->item(0)->nodeValue;
-           $animeA = $data->getElementsByTagName("anime_author")->item(0)->nodeValue;
-           $animeD = $data->getElementsByTagName("anime_description")->item(0)->nodeValue;
-           echo "<ul><h2>$animeT</h2>
-                    <ul>
-                        <li>Author: $animeA </li>
-                        <li>Description: $animeD</li>
-                        
-                    </ul>
-                </ul>";
-              }
-?>
+</ul>
