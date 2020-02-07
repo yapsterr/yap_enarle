67a75ab0170fc8f5e5b2333c84987bb6629a0b6e
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-07 18:36:30 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,25 +1,25 @@
-<?php
+<?php 
 
-    $rssfeed = '<?xml version="1.0" encoding="UTF-8"?>';
-    $rssfeed .= '<rss version="2.0">';
-    $rssfeed .= '<channel>';
+$rss= '<?xml version="1.0" encoding="UTF-8"?>';
+$rss .= '<rss version="2.0">';
+$rss .= '<channel>';
 
-    $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820335") or die(mysqli_error($connect));
-    $sql = "SELECT * FROM tblanime";
-    $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
+$connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820335") or die (mysqli_error($connect));
+$sql = "SELECT * FROM tblanime";
+$query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
-    while($row=mysqli_fetch_assoc($query)) {
-        extract($row);
 
-        $rssfeed .= '<Anime>';
-        $rssfeed .= '<title>' . $animeT . '</title>';
-        $rssfeed .= '<author>' . $animeA . '</author>';
-        $rssfeed .= '<description>' . $animeD . '</description>';
-        $rssfeed .= '</Anime>';
-    }
+while($record= mysqli_fetch_assoc($query)) {
+    extract($record);
+    
+    $rss .= '<anime>';
+    $rss .= '<title>' . $title . '</title>';
+    $rss .= '<author>' . $author . '</author>';
+    $rss .= '<description>' . $description . '</description>';
+    $rss .= '</anime>';
+}
+$rss .= '</channel>';
+$rss .= '</rss>';
 
-    $rssfeed .= '</channel>';
-    $rssfeed .= '</rss>';
-
-    echo $rssfeed;
-?>    
+echo $rss; 
+?>
