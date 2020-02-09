7e2ddb2724042bccf9516433ce3216a89ce8f135
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-09 16:31:54 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -5,18 +5,19 @@ $rss .= '<rss version="2.0">';
 $rss .= '<channel>';
 
 $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820335") or die (mysqli_error($connect));
-$sql = "SELECT * FROM netflix";
+$sql = "SELECT * FROM tblbrowser";
 $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
 
 
 while($record= mysqli_fetch_assoc($query)) {
     extract($record);
     
-    $rsse .= '<netflix>';
-    $rsse .= '<title>' . $title. '</title>';
-    $rsse .= '<actors>' . $actors . '</actors>';
-    $rsse .= '<information>' . $information . '</information>';
-    $rsse .= '</netflix>';
+    $rss .= '<web>';
+    $rss .= '<browser>' . $browser. '</browser>';
+    $rss .= '<creator>' . $creator . '</creator>';
+    $rss .= '<publish>' . $publish . '</publish>';
+    $rss .= '<language>' . $language . '</language>';
+    $rss .= '</web>';
 }
 $rss .= '</channel>';
 $rss .= '</rss>';
