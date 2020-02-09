b6148b2b37878c97e8629bb19f60f9633f72ee8e
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-09 17:20:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
  $content = $domOBJ->getElementsByTagName("movie");
  
  ?>
-<h2> Web Browsers </h2>
+<h2> The Best Rom-Coms Netflix Has to Offer </h2>
+<p> Rom-coms can get a bad rep. There's nothing wrong with a light-hearted film with a strong female lead and
+an emotional arc—we all need a good rom-com for self-care purposes sometimes!—but it's true that a poorly made rom-com can veer into gimmicky territory. 
+So it's a good thing that we're here to pick out the best ones that also happen to be on Netflix right now.
+Even the biggest rom-com critics can't argue with these picks; they're a shot of pure joy and will leave you feeling that maybe, just maybe, everything will work out in the end.
+All you need is a Netflix subscription. And probably a small country's worth of junk food.
+</p>
  <ul>
     <?php
  foreach( $content as $data )
@@ -14,10 +20,10 @@
    $actors = $data->getElementsByTagName("Actors")->item(0)->nodeValue;
    $detail = $data->getElementsByTagName("Detail")->item(0)->nodeValue;
   
-   echo "<li><b>Browser Name:</b> $title
+   echo "<li><b>Movie Name:</b> $title
             <ul>
-                <li> <b>The Creator:</b> $actors</li>
-                <li> <b>Published:</b> $detail</li>
+                <li> <b>Starring:</b> $actors</li>
+                <li> <b>About the Movie:</b> $detail</li>
             </ul>
         </li>";
  }
