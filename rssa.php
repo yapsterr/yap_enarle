cef429bb7856685009b485b90da17e2516502f4f
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-02-09 18:45:03 +0800

    Update rssa.php

--- rssa.php
+++ rssa.php
@@ -14,9 +14,9 @@ while($r= mysqli_fetch_assoc($query)) {
     
     $rssa .= '<movie>';
     $rssa .= '<number>' . $number. '</number>';
-    $rssa .= '<eltit>' . $title. '</eltit>';
-    $rssa .= '<srotca>' . $actors . '</srotca>';
-    $rssa .= '<ofni>' . $details . '</ofni>';
+    $rssa .= '<Head>' . $title. '</Head>';
+    $rssa .= '<Star>' . $actors . '</Star>';
+    $rssa .= '<List>' . $details . '</List>';
     $rssa .= '</movie>';
 }
 $rssa .= '</channel>';
